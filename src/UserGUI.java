029ec390f3760a52865029ae326c40f5888f9d87
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2020-12-16 21:00:15 +0000

    Added empty GUI

--- src/UserGUI.java
+++ src/UserGUI.java
@@ -1,2 +1,19 @@
-public class UserGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class UserGUI extends JFrame {
+    private JPanel mainPanel;
+
+    public UserGUI() {
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500,500));
+        pack();
+    }
+
+
+    public static void main(String[] args) {
+        UserGUI page = new UserGUI();
+        page.setVisible(true);
+    }
 }
