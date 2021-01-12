d077bb08556458ec3347c3621d3a46d64b7ee0f1
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-01-12 16:11:50 +0000

    Redesigned card payment gui

--- src/view/UserGUI.java
+++ src/view/UserGUI.java
@@ -29,18 +29,7 @@ public class UserGUI extends JFrame {
     private JPanel itemSelectPanel;
     private JPanel cardPaymentPanel;
     private JPanel NumPadJPanel;
-    private JButton btn1;
-    private JButton btn2;
-    private JButton btn8;
-    private JButton btn7;
-    private JButton btn4;
-    private JButton btn6;
-    private JButton btn5;
-    private JButton btn9;
     private JButton btnGo;
-    private JButton btn0;
-    private JButton btnBackspace;
-    private JButton btn3;
     private JTextField tFieldPin;
     private JButton btnCancelCard;
     private JPanel cashPaymentPanel;
